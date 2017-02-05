4c5661b66436338e2d6eeb9e4e225a619a44131a
Mclaren <liuhao@chinam2b.com>
2017-02-05 15:36:57 +0800

    refactor

--- src/main/scala-2.11/com/tvc/wish/product/ActorsPool.scala
+++ src/main/scala-2.11/com/tvc/wish/product/ActorsPool.scala
@@ -6,7 +6,6 @@ import java.util.UUID
 
 private[product] object ActorsPool {
   private[this] val system = ActorSystem("MySystem")
-  private[this] val actor = system.actorOf(Props[WishProductFetcherActor], name = "wishActor")
 
   def retrieveActor = system.actorOf(Props[WishProductFetcherActor], name = UUID.randomUUID().toString)
 
